639cd41ee365cea742726e58e69e75f3b3230ce1
h47r15 <hatris@icloud.com>
2022-05-02 22:43:05 +0200

    header update

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,25 +1,27 @@
 <div class="navbar navbar-inverse navbar-fixed-top">
       <div class="navbar-inner">
         <div class="container">
-          <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-        
+
+
           <a class="brand" href="/" style="margin-left: 0px; padding-left: 8px;"><h1 style="color:#333">Pixie Bears</h1></a>
-          
+
           <a class="socialnav" href="https://twitter.com/PixieBearsSOL">
             <img data-src="holder.js/300x200" alt="300x200" src="/public/img/twitteretro.gif" style="width: 40px; height: 40px; filter: none !important;">
           </a>
-          
+
             <a class="socialnav"  href="https://discord.gg/ewpGHAZBus">
             <img data-src="holder.js/300x200" alt="300x200" src="/public/img/discord.webp" style="width: 40px; height: 40px; filter: none !important;">
           </a>
           <a class="socialnav" href="https://www.instagram.com/pixiebearsSOL/">
             <img data-src="holder.js/300x200" alt="300x200" src="/public/img/instagram.png" style="width: 40px; height: 40px; filter: none !important;">
           </a>
-          
+
+          <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+
           <div class="nav-collapse collapse">
             <ul class="nav">
               <li class="active"><a href="/#home">Home</a></li>
