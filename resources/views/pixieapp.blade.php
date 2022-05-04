578844709f3e18e781360888877a1314ce4b1f27
h47r15 <hatris@icloud.com>
2022-05-04 19:43:59 +0200

    4k px

--- resources/views/pixieapp.blade.php
+++ resources/views/pixieapp.blade.php
@@ -311,10 +311,10 @@
     @include('header')
 
     <div style="background-image: url('/img/BGBlue.png');
-    height: 1730px;position: absolute;width: 100%;z-index: 0;" class="rellax" data-rellax-speed="-3"></div>
+    height: 4000px;position: absolute;width: 100%;z-index: 0;" class="rellax" data-rellax-speed="-3"></div>
 
 <div style="background-image: url('/img/BGYellow.png');
-height: 1730px;position: absolute;width: 100%;z-index: 0;padding-top:600px;" class="rellax" data-rellax-speed="4"></div>
+height: 4000px;position: absolute;width: 100%;z-index: 0;padding-top:600px;" class="rellax" data-rellax-speed="4"></div>
 
 
     <div class="container" style="z-index: 20;position: relative;">
