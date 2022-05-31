6c50dcd50386032b0d9747044295ae73466611f8
h47r15 <hatris@icloud.com>
2022-05-31 13:36:42 +0200

    official partners

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,8 @@
 import '../assets/css/bootstrap-custom.scss';
 import { Container, Row } from "react-bootstrap";
 import { Title } from "./core";
+import NftCalendar from "../assets/icons/NftCalendar";
+
 
 function Footer() {
 
@@ -10,7 +12,18 @@ return (
     <footer className='text-center my-5'>
     <Container>
             <div className="text-center">
-                <Row className="d-inline-block p-3 mb-4">
+            <Title variant="secSm" css={` font-size: 0.8125rem; margin-bottom:1rem; `} className="p-3 mb-4">
+                Official Partners
+            </Title>
+
+            <Row className="d-inline-block p-3 mb-4 w-100">
+                <a className="col nav-item ps-3" target="_blank" rel="noreferrer" href="https://nftcalendar.io"> 
+                     <NftCalendar />
+                </a>
+            </Row>
+
+
+                <Row className="d-inline-block p-3 mb-4 w-100">
                     <a className="col nav-item ps-3" target="_blank" rel="noreferrer" href="https://www.instagram.com/pixiebears_SOL/"> 
                         <svg className="nav-icon" fill="#000000" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 50 50" width="20px" height="20px">    <path d="M 16 3 C 8.83 3 3 8.83 3 16 L 3 34 C 3 41.17 8.83 47 16 47 L 34 47 C 41.17 47 47 41.17 47 34 L 47 16 C 47 8.83 41.17 3 34 3 L 16 3 z M 37 11 C 38.1 11 39 11.9 39 13 C 39 14.1 38.1 15 37 15 C 35.9 15 35 14.1 35 13 C 35 11.9 35.9 11 37 11 z M 25 14 C 31.07 14 36 18.93 36 25 C 36 31.07 31.07 36 25 36 C 18.93 36 14 31.07 14 25 C 14 18.93 18.93 14 25 14 z M 25 16 C 20.04 16 16 20.04 16 25 C 16 29.96 20.04 34 25 34 C 29.96 34 34 29.96 34 25 C 34 20.04 29.96 16 25 16 z"/></svg>
                     </a>
